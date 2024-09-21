263c6b0326b708b0e15dc5619db0d90a4e80f8b3
paintedlbird7 <rosaperezlarios@gmail.com>
2024-09-21 14:50:05 -0700

    proj 8 completed

--- features/HW8filterLoops.feature
+++ features/HW8filterLoops.feature
@@ -1,11 +1,15 @@
 # Created by rosaperez at 9/12/24
 Feature: HM4filter
 
-Scenario: Filter by Dress Length
+Scenario: Filtering by multiple filters
     Then open ebay.com
     Then In search field type "dress"
     Then Click the "Search" button
     Then filter "Dress Length" option "Short"
-    And All items should be related to "Dress Length" value "Short"
+    Then filter "Brand" option "Calvin Klein"
+    And All items should be related to following
     # uncomment to test exception, expected Short, received/actual Long
 #    And All items should be related to "Dress Length" value | "Long"
+        | filter name  | filter value      |
+        | Brand        | Calvin Klein    |
+        | Dress Length | Short           |
--- features/steps/HW8filterLoops.py
+++ features/steps/HW8filterLoops.py
@@ -8,11 +8,13 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from pprint import pprint as pp
 
-#
+# completed
+
 @step('In search field type "{item}"')
 def search_for_some_item(context, item):
     search_field = context.driver.find_element('xpath', "//input[@aria-label = 'Search for anything']")
     search_field.send_keys(item)
+
 
 @step('Filter "{filter_name}" option "{option}"')
 def filter_items(context, filter_name, option):
@@ -21,16 +23,27 @@ def filter_items(context, filter_name, option):
                                                 f"//div[@class = 'x-refine__select__svg'][.//span[text() = '{option}']]//input")
     filter_option.click()
 
+# task: filters that are not directly displayed on the item page, should be verified
+# 1.first thing to do is collect all items
+# repeat same action over over again, looping all items
+# 2. some actions repeated 50 -58 ttimes
+# 3. open every item in a new tab
+# 3.1 click or extract href and excute script
+
 @step('All items should be related to "{spec_criteria}" value | "{spec_value}"')
 def check_filters(context, spec_criteria, spec_value):
-    #collect all items
-    all_items = context.wait.until(EC.presence_of_all_elements_located((By.XPATH, "//li[@class = 's-item s-item__pl-on-bottom' and contains(@id, 'item')]")))
+    # collect all items
+    all_items = context.wait.until(EC.presence_of_all_elements_located(
+        (By.XPATH, "//li[@class = 's-item s-item__pl-on-bottom' and contains(@id, 'item')]")))
     # how to operate with handles
-    main_page = context.driver.window_handles # []
+    main_page = context.driver.window_handles  # []
     # print(context.driver.window_handles) # []
 
     context.driver.switch_to.window()
-
+    # 4. check that we do have the brand is on the item specs if not raise an exception or
+    # 4.1 collect all errors, if errors raise-signal if anything is found
+    # 4.2
+    errors = []
     for item in all_items:
         # get to every item page
         ## get the destination link
@@ -44,28 +57,39 @@ def check_filters(context, spec_criteria, spec_value):
         # switch to last window
         context.driver.current_window_handle(context.driver.window_handles[-1])
         # collect all item specs
-        lbls = context.wait.until(EC.presence_of_all_elements_located(By.XPATH, "//dt[@class = 'ux-labels-values__labels']"))
-        vals  = context.wait.until(EC.presence_of_all_elements_located(By.XPATH, "//dd[@class = 'ux-labels-values__values']"))
 
-        lbls_text = []
-        for lbl in lbls:
-            lbls_text.append(lbl.text)
 
-        vals_text = []
-        for val in vals:
-            vals_text.append(val.text)
+        lbls_text = [l.text for l in context.wait.until((EC.presence_of_all_elements_located(By.XPATH, "//dt[@class = 'ux-labels-values__labels']")))]
+        vals_text = [v.text for v in context.wait.until(EC.presence_of_all_elements_located((By.XPATH, "//dd[@class = 'ux-labels-values__values']")))]
+
+
+        lables_text = [lbl.text for lbl in context.wait(...)]
+        lables_text = [x.text for x in context.wait.until(...)]
 
         item_specs = dict(zip(lbls_text, vals_text))
         pp(item_specs)
 
+        for exp_f, exp_v in expected_specs.items():
+            if exp_f not in item_specs.keys():
+                errors.append(f'Item {link_title} does not have specification related to {exp_f}')
+            elif exp_v.lower() != item_specs[exp_f].lower():
+                errors.append(f'Items {link_title} has different {exp_f}.{item_specs[exp_f]} != {exp_v}')
+
         # validation
-        if item_specs[spec_criteria] != spec_value:
-            raise ValueError(f'Item [{link_title}] not matching by {spec_criteria}!'
-                             f'\n\texpected - {spec_value}'
-                             f'n\tactual - {item_specs[spec_criteria]}')
 
-
-
-        # get back to main content
+        # 4.2 get back to main content
         context.driver.close()
         context.driver.current_window_handle(main_page)
+
+@step('All items should be related to following')
+def all_items_many_filters_check(context):
+    expected_specs = {}
+    # expected_specs = {r['filter name']:r['value' for r in context.table]}
+
+    for row in context.table:
+        name = row['filter name']
+        value = row['filter value']
+        expected_specs[name] = value
+        print(expected_specs)
+
+
